131f0c9b6de0e52203b0a981973ef4edd23239a1
Charlie LEGER <c.leger@borea-dental.com>
2024-09-25 10:26:23 +0200

    [log] Add single pair files
    This repository is mainly used to copy/paste classes (and provide instructions), so provide a single pair of header/source to be easier to integrate

---
+++ lib/logs_single_file/abstractlogger.cpp
@@ -0,0 +1,52 @@
+#include "abstractlogger.h"
+
+/*****************************/
+/* Class documentations      */
+/*****************************/
+
+/*****************************/
+/* Start namespace           */
+/*****************************/
+
+namespace libra
+{
+
+/*****************************/
+/* Static initialization     */
+/*****************************/
+ILogger* LogManager::m_refLogger = nullptr;
+
+/*****************************/
+/* Functions implementation  */
+/*        LogManager         */
+/*****************************/
+
+void LogManager::setLogger(ILogger *logger)
+{
+    m_refLogger = logger;
+}
+
+void LogManager::registerLog(ILogger::Level level, const char *filename, int lineNumber, const char *fctName, const std::string &msg)
+{
+    /* Verify that logger interface is valid */
+    if(!m_refLogger){
+        return;
+    }
+
+    /* Set context informations */
+    const LogContext ctx(filename, lineNumber, fctName);
+
+    /* Register log */
+    m_refLogger->log(level, ctx, msg);
+}
+
+
+/*****************************/
+/* End namespace             */
+/*****************************/
+
+} // namespace libra
+
+/*****************************/
+/* End file                  */
+/*****************************/
